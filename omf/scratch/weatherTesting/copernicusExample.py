4657bf944397fc01686ade8d79202ab940770ffa
jenny <jenthakkar@gmail.com>
2025-11-12 04:00:40 -0500

    Initial commit - copying files frmo previous fork

--- omf/scratch/weatherTesting/copernicusExample.py
+++ omf/scratch/weatherTesting/copernicusExample.py
@@ -3,10 +3,8 @@ from omf.models import __neoMetaModel__
 from pathlib import Path
 
 # I made a model called copernicusTest, and ran it to get the data.
-modelDir = Path(__neoMetaModel__._omfDir, "data", "Model", "admin", "copernicusTest")
-defaultCSVFileName = "output_cdsWeatherDataFull.csv"
-weather_ds_df = weather.cds_processWeatherData(modelDir=modelDir)
-ac_dc_df = weather.cds_getSolar(Path(modelDir, defaultCSVFileName))
+modelDir = Path(__neoMetaModel__._omfDir, "scratch", "weatherTesting")
+cdsFile = Path(modelDir, "output_cdsWeatherDataFull.csv")
+#weather_ds_df = weather.cds_processWeatherData(modelDir=modelDir)
+ac_dc_df = weather.cds_pySAM_getSolar(cdsFile)
 print(ac_dc_df.head)
-# ds = weather.cds_getWind(weather_ds_df[0])
-# print(ds)
