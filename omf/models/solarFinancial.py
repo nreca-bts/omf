a99b580d45952af4dc853912503772c5d7485452
jenny <jenthakkar@gmail.com>
2026-02-11 00:04:35 -0500

    nrelsam2013 - removed solver and imports frmo solarFinancial.py

--- omf/models/solarFinancial.py
+++ omf/models/solarFinancial.py
@@ -10,7 +10,6 @@ from math import prod
 
 # OMF imports
 from omf import weather
-from omf.solvers import nrelsam2013
 from omf.models import __neoMetaModel__
 from omf.models.__neoMetaModel__ import *
 
