a2a8ebed66862df8e5f88bf0be1713f087f021f8
dpinney <david@pinney.org>
2026-05-20 18:43:03 -0400

    remove opendsscmd install

--- install.py
+++ install.py
@@ -27,7 +27,6 @@ if major_platform == "Linux" and "ubuntu" in linux_distro:
 	# os.system(f"{sys.executable} -m pip install --upgrade pip setuptools")
 	os.system(f"{sys.executable} -m pip install --ignore-installed -r {source_dir}/requirements.txt")
 	os.system(f"{sys.executable} -m pip install --ignore-installed -e {source_dir}")
-	os.system(f'sudo chmod 755 {source_dir}/omf/solvers/opendss/opendsscmd-1.7.4-linux-x64-installer.run && sudo {source_dir}/omf/solvers/opendss/opendsscmd-1.7.4-linux-x64-installer.run --mode unattended')
     # - If using Docker, this configuration should be done in the Dockerfile
 	print('*****\nRun $ export LC_ALL=C.UTF-8 $ if running phaseId._tests() gives an ascii decode error.\n*****')
 elif major_platform == "Linux" and "ubuntu" not in linux_distro:
@@ -44,7 +43,6 @@ elif major_platform == "Linux" and "ubuntu" not in linux_distro:
 	os.system(f"{sys.executable} -m pip install --upgrade pip")
 	os.system(f"{sys.executable} -m pip install -r {source_dir}/requirements.txt")
 	os.system(f"{sys.executable} -m pip install -e {source_dir}")
-	os.system(f'sudo chmod 755 {source_dir}/omf/solvers/opendss/opendsscmd-1.7.4-linux-x64-installer.run && sudo {source_dir}/omf/solvers/opendss/opendsscmd-1.7.4-linux-x64-installer.run --mode unattended')
     # - If using Docker, this configuration should be done in the Dockerfile
 	print('*****\nRun $ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib $ if opendsscmd gives a shared library error.\n*****')
 elif major_platform == 'Windows':
@@ -73,8 +71,6 @@ elif major_platform == "Darwin": # MacOS
 	''') # sed is to hack the build to work without user input.
 	os.system(f"{sys.executable} -m pip install -r {source_dir}/requirements.txt")
 	os.system(f"{sys.executable} -m pip install -e {source_dir}")
-	os.system(f'sudo hdiutil attach {source_dir}/omf/solvers/opendss/opendsscmd-1.7.4-osx-installer.dmg')
-	os.system('open /Volumes/OpenDSS/opendsscmd-1.7.4-osx-installer.app')
 	print('Please go to System Preferences to finish installing OpenDSS on Mac')
 else:
 	print("Your operating system is not currently supported. Platform detected: " + str(platform.system()) + str(platform.linux_distribution()))
