7fe56cbd44b3500adc1491469a459da5fd926782
jenny <jenthakkar@gmail.com>
2026-05-27 16:50:48 -0400

    Fixed nsrdb api url typo

--- omf/weather.py
+++ omf/weather.py
@@ -1510,9 +1510,9 @@ def nsrbd_latlon_to_wkt(longitude, latitude):
     return f"POINT({lon_str} {lat_str})"
 
 
-def nrl_get_nsrdb_data(data_set: str, longitude: float, latitude: float, year: int, api_key, attributes=[], utc='true', leap_day='false', email='admin@omf.coop', interval=None, filename=None):
+def nlr_get_nsrdb_data(data_set: str, longitude: float, latitude: float, year: int, api_key, attributes=[], utc='true', leap_day='false', email='admin@omf.coop', interval=None, filename=None):
 	'''Create nsrdb factory and execute query. Optional output to file or return the response object.'''
-	base_url = 'https://developer.nrl.gov'
+	base_url = 'https://developer.nlr.gov'
 	request_url = ""
 	params = {}
 	params['api_key'] = api_key
